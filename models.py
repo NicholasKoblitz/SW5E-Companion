8c77ff87ad0a68b3ccde852f49f2944e39ee5b7c
NicholasK <nicholask1@outlook.com>
2022-03-06 09:34:12 -0500

    Created class table and associated tables

--- models.py
+++ models.py
@@ -12,12 +12,14 @@ def connect_db(app):
     db.init_app(app)
 
 
+# User and Character Tables
+
 class User(db.Model):
     """Users Table"""
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
 
@@ -27,7 +29,7 @@ class Character(db.Model):
 
     __tablename__ = "characters"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.Column(db.Text, nullable=False)
     class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
@@ -79,10 +81,130 @@ class Character(db.Model):
     languages = db.Column(db.Text, nullable=False)
 
 
+# Classes table and all associated tables
+
 class Class(db.Model):
     """Classes Table"""
 
     __tablename__ = "classes"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, nullable=False)
+    intro = db.Column(db.Text, nullable=False)
+    description_1_name = db.Column(db.Text, nullable=False)
+    description_1_info = db.Column(db.Text, nullable=False)
+    description_2_name = db.Column(db.Text, nullable=False)
+    description_2_info = db.Column(db.Text, nullable=False)
+    hit_dice = db.Column(db.Text, nullable=False)
+    hit_points_at_1st_level = db.Column(db.Text, nullable=False)
+    hit_ponits_at_higher_levels = db.Column(db.Text, nullable=False)
+    armor_proficiencies = db.Column(db.Text, nullable=False)
+    weapon_proficiencies = db.Column(db.Text, nullable=False)
+    tool_proficiencies = db.Column(db.Text, nullable=False)
+    saving_throw_proficiencies = db.Column(db.PickleType, nullable=False)
+    skills = db.Column(db.Text, nullable=False)
+    starting_equipment = db.Column(db.Text, nullable=False)
+    variant_starting_wealth = db.Column(db.Text, nullable=False)
+    description_by_level = db.Column(db.PickleType, nullable=False)
+
+
+class ClassFeatures(db.Model):
+    """Class Features Table"""
+
+    __tablename__ = "class_features"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    details = db.Column(db.Text, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+class Archetype(db.Model):
+    """Archetypes table"""
+
+    __tablename__ = "archetypes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    caster_type = db.Column(db.Text, nullable=False)
+    features = db.Column(db.PickleType, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+class FightingStyles(db.Model):
+    """Fighting Styles Table"""
+
+    __tablename__ = "fighting styles"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    aspects = db.Column(db.PickleType, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+class FightingMastery(db.Model):
+    """Fighting Mastery Table"""
+
+    __tablename__ = "fighting masteries"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    aspects = db.Column(db.PickleType, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+class LightsaberForms(db.Model):
+    """Lightsaber Forms Table"""
+
+    __tablename__ = "lightsaber forms"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prerequiste = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+class TechPowers(db.Model):
+    """Tech-Power Table"""
+
+    __tablename__ = "tech powers"
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    force_alignment = db.Column(db.Text, nullable=False)
+    casting_period = db.Column(db.Text, nullable=False)
+    range = db.Column(db.Text, nullable=False)
+    duration = db.Column(db.Text, nullable=False)
+    concentration = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+    
+
+class ForcePowers(db.Model):
+    """Force Powers Table"""
+
+    __tablename__ = "force powers"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False)
+    level = db.Column(db.Integer, nullable=False)
+    force_alignment = db.Column(db.Text, nullable=False)
+    casting_period = db.Column(db.Text, nullable=False)
+    range = db.Column(db.Text, nullable=False)
+    duration = db.Column(db.Text, nullable=False)
+    concentration = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.id"))
+
+
+# Speces and associated tables
+
+class Specie(db.Model):
+    """Species Table"""
+
+    __tablename__ = "species"
+
+    id = db.Column(db.Integer, primary_key=True,  autoincrement=True)
