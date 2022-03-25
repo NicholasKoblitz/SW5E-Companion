eeaba5cf330c70774db9d294b263ac71353de39f
NicholasK <nicholask1@outlook.com>
2022-03-25 18:57:51 -0400

    Added form validation

--- forms.py
+++ forms.py
@@ -1,34 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Length
 
 class SignupForm(FlaskForm):
     """User's sign up form"""
 
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("Username", validators=[InputRequired(message="Must enter username")])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8, message="Password must be at least 8 characters long")])
 
 class LoginForm(FlaskForm):
     """User's Login Form"""
 
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("Username", validators=[InputRequired(message="Must enter username")])
+    password = PasswordField("Password", validators=[InputRequired(message="Must enter password")])
 
 class AbilityScoresForm(FlaskForm):
     """Character Ability Scores Form"""
 
-    strength = SelectField("Strength", choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
-    dexterity = SelectField("Dexterity", choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
-    constitution = SelectField("Constitution", choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
-    intelligence = SelectField('Intelligence', choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
-    wisdom = SelectField("Wisdom", choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
-    charisma = SelectField("Charisma", choices=[(15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    strength = SelectField("Strength", choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    dexterity = SelectField("Dexterity", choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    constitution = SelectField("Constitution", choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    intelligence = SelectField('Intelligence', choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    wisdom = SelectField("Wisdom", choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
+    charisma = SelectField("Charisma", choices=[(None, "Select Score"), (15,15), (14,14), (13,13), (12,12), (10,10), (8,8)])
 
 
 class DescriptionForm(FlaskForm):
     """Character Description Form"""
 
     name = StringField("Name")
-    # image_url = StringField("Image URL")
     alignment = SelectField("Choose Charatcer Alignment", choices=[
         ("Lawful Light", "Lawful Light"), 
         ("Neutral Light", "Neutral Light"), 
@@ -40,11 +40,6 @@ class DescriptionForm(FlaskForm):
         ("Neutral Dark", "Neutral Dark"), 
         ("Chaotic Dark", "Chaotic Dark")
         ])
-    # background = SelectField("Backgrounds", choices=[
-        # ("Agent", "Agent"), 
-        # ("Bounty Hunter", "Bounty Hunter"), 
-        # ("Criminal", "Criminal")
-        # ])
     personality_trait = StringField("Personality Traits")
     ideal = StringField("Ideal")
     bond = StringField("Bond")
