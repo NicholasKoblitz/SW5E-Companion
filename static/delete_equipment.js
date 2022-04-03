59ffab3ec87b53e520d0a51f05b1d42d6f9ae242
NicholasK <nicholask1@outlook.com>
2022-04-03 17:59:58 -0400

    Worked on equipmnet delete btn.

--- static/delete_equipment.js
+++ static/delete_equipment.js
@@ -1,17 +1,9 @@
 const btn = document.querySelectorAll(".delete-btn")
 
-await function deleteEquipment(evt) {
-    console.dir(evt.target)
-    evt.target.parentNode.remove()
-    axios({
-        method: "post",
-        url: "/character/equipment/delete",
-        data: {
-            
-        }
-    })
+async function deleteEquipment(evt) {
+    console.dir(evt.target.parentNode.childNodes)
+    evt.target.parentNode.childNodes[1].remove()
 }
-
 
 for(let i of btn) {
     i.addEventListener("click", deleteEquipment)
