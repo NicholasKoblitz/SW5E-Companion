086582341d908406f64de3a58971b17724563c02
NicholasK <nicholask1@outlook.com>
2022-04-07 21:12:48 -0400

    fixed syntax error

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ GEAR = []
 
 app = Flask(__name__)
 
-uri = os.getenv("DATABASE_URI")  # or other relevant config var
+uri = os.environ.get("DATABASE_URI")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
