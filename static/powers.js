6846d75b7a805f5089830d516f84bf661b57b8bc
NicholasK <nicholask1@outlook.com>
2022-04-11 21:37:55 -0400

    Added power identification based on power level and force alignment:

--- static/powers.js
+++ static/powers.js
@@ -1,49 +1,104 @@
 const alignment = document.querySelector("#alignment")
+const techPowers = document.querySelector("#tech-powers")
 const powerSelection = document.querySelectorAll(".power-selection")
-// console.log(powerSelection)
-
-// for(let i of powerSelection) {
-//     console.log(i)
-    // for(let j of i.options) {
-    //     if(j.classList[0] == 'dark') {
-    //         console.log(j)
-    //         j.hidden = true
-    //     }
-    // }
+const powerSelectionTech = document.querySelectorAll(".power-selection-tech")
 
 
-function align(evt) {
+if(alignment !== null) {
+    function align(evt) {
 
-    if(evt.target.value == "universal") {
-        for(let item of powerSelection) {
-            for(let i of item.options) {
-                i.hidden = false
-                    if("universal" != i.classList[0]) {
-                        i.hidden = true
-                }
-            } 
+        if(evt.target.value === "universal-0") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("universal-0" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "light-0") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("light-0" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "dark-0") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("dark-0" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "universal-1") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("universal-1" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "light-1") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("light-1" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "dark-1") {
+            for(let item of powerSelection) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("dark-1" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
         }
     }
-    else if(evt.target.value == "light") {
-        for(let item of powerSelection) {
-            for(let i of item.options) {
-                i.hidden = false
-                    if("light" != i.classList[0]) {
-                        i.hidden = true
-                }
-            } 
+    
+    alignment.addEventListener("change", align)
+}
+else if(alignment === null) {
+    function tech(evt) {
+        if(evt.target.value === "at-will") {
+            for(let item of powerSelectionTech) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("at-will" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
+        }
+        else if(evt.target.value === "1st") {
+            for(let item of powerSelectionTech) {
+                for(let i of item.options) {
+                    i.hidden = false
+                        if("1st" !== i.classList[0]) {
+                            i.hidden = true
+                    }
+                } 
+            }
         }
     }
-    else if(evt.target.value == "dark") {
-        for(let item of powerSelection) {
-            for(let i of item.options) {
-                i.hidden = false
-                    if("dark" != i.classList[0]) {
-                        i.hidden = true
-                }
-            } 
-        }
-    }
+    
+    techPowers.addEventListener("change", tech)
 }
 
-alignment.addEventListener("change", align)
+
+
+
+
